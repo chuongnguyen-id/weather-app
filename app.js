9f38b2c58387be662498d0a2ffcb25eba9efb232
chuongnguyen-id <chuongnguyen.dc@gmail.com>
2023-03-19 22:23:24 +0700

    weather-app

---
+++ app.js
@@ -0,0 +1,65 @@
+const asyncRequest = require("async-request");
+
+const getWeather = async (location) => {
+  const access_key = "f29b4a0a3b56a4ae465cac274b778074";
+  const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${location}`;
+  try {
+    const res = await asyncRequest(url);
+    const data = JSON.parse(res.body);
+    const weather = {
+      isSuccess: true,
+      region: data.location.region,
+      country: data.location.country,
+
+      temperature: data.current.temperature,
+      wind_speed: data.current.wind_speed,
+      precip: data.current.precip,
+      cloudcover: data.current.cloudcover,
+    };
+    console.log(weather);
+    return weather;
+  } catch (error) {
+    console.log(error);
+    return {
+      isSuccess: false,
+      error,
+    };
+  }
+};
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const pathPublic = path.join(__dirname, "./public");
+app.use(express.static(pathPublic));
+
+// http://localhost:7000/
+app.get("/", async (req, res) => {
+  const params = req.query;
+  const location = params.address;
+  const weather = await getWeather(location);
+  console.log(weather);
+  if (location) {
+    res.render("weather", {
+      status: true,
+      region: weather.region,
+      country: weather.country,
+      temperature: weather.temperature,
+      wind_speed: weather.wind_speed,
+      precip: weather.precip,
+      cloudcover: weather.cloudcover,
+    });
+  } else {
+    res.render("weather", {
+      status: false,
+    });
+  }
+});
+
+app.set("view engine", "hbs");
+
+const port = 7000;
+app.listen(7000, () => {
+  console.log(`app run on http://localhost:${port}`);
+});
